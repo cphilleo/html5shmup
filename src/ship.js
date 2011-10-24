09f69d1056cb41a9e0050b8a560e9bf202318fec
Collyn Philleo <jaster32@gmail.com>
2011-10-23 20:28:32 -0700

    Added ship colliding with asteroids

--- src/ship.js
+++ src/ship.js
@@ -1,77 +1,99 @@
 var Ship = function() {
-	this.x = (640 /2) - 20;
-	this.y = 480 - 50;
-	this.speed = 250;
-	this.shotTimer = 0;
-	this.state = "forward";
-	this.sprite = new Sprite(images[2], 39, 43);
-	this.sprite.addAnimation({name: "forward", speed: 0.1, frames: [1,2], loop: true});
-	this.sprite.addAnimation({name: "left", speed: 0.1, frames: [4,5], loop: true});
-	this.sprite.addAnimation({name: "right", speed: 0.1, frames: [7,8], loop: true});
-	this.sprite.playAnimation("forward");
-}
-
-Ship.prototype.render = function() {
-	this.sprite.draw(this.x, this.y);
-}
-
-Ship.prototype.update = function(delta) {
-	this.sprite.update(delta);
-	this.shotTimer -= 100 * delta;
+	//public vars
 	
-	if (KEY_LEFT in keysDown) {
-		if (this.x > 0) {
-			this.x -= this.speed * delta;
+	//private vars
+	var _x = (640 /2) - 20;
+	var _y = 480 - 50;
+	var _width = 39;
+	var _height = 43;
+	var _speed = 250;
+	var _shotTimer = 0;
+	var _state = "forward";
+	var _sprite = new Sprite(images[2], _width, _height);
+	
+	//public methods
+	this.render = function() {
+		_sprite.draw(_x, _y);
+	}
+	
+	this.update = function(delta) {
+		_sprite.update(delta);
+		_shotTimer -= delta;
+		
+		//collisions
+		if (asteroidManager.checkCollision(_x, _y, _x + _width, _y + _height)) {
+			g_explosionManager.addExplosion(_x, _y);
+			g_score = 0;
+			_x = (640 / 2) - 20;
+			_y = 480 - 50;
 		}
 		
-		if (this.state === "forward") {
-			this.sprite.playAnimation("left");
-			this.state = "left";
-		}
-	}
-	
-	else {
-		if (this.state === "left") {
-			this.sprite.playAnimation("forward");
-			this.state = "forward";
-		}
-	}
-	
-	if (KEY_RIGHT in keysDown) {
-		if (this.x + 39 < 640) {
-			this.x += this.speed * delta;
+		//handle input
+		if (KEY_LEFT in keysDown) {
+			if (_x > 0) {
+				_x -= _speed * delta;
+			}
+			
+			if (_state === "forward") {
+				_sprite.playAnimation("left");
+				_state = "left";
+			}
 		}
 		
-		if (this.state === "forward") {
-			this.sprite.playAnimation("right");
-			this.state = "right";
+		else {
+			if (_state === "left") {
+				_sprite.playAnimation("forward");
+				_state = "forward";
+			}
+		}
+		
+		if (KEY_RIGHT in keysDown) {
+			if (_x + 39 < 640) {
+				_x += _speed * delta;
+			}
+			
+			if (_state === "forward") {
+				_sprite.playAnimation("right");
+				_state = "right";
+			}
+		}
+		
+		else {
+			if (_state === "right") {
+				_sprite.playAnimation("forward");
+				_state = "forward"
+			}
+		}	
+		
+		if (KEY_UP in keysDown) {
+			if (_y > 0) {
+				_y -= _speed * delta;
+			}
+		}
+		
+		if (KEY_DOWN in keysDown) {
+			if (_y + 36 < 480) {
+				_y += _speed * delta;
+			}
+		}
+		
+		if (KEY_SPACE in keysDown) {
+			if (_shotTimer <= 0) {
+				_shotTimer = 0.25;
+				bulletManager.addBullet(_x + 7, _y + 9);
+				bulletManager.addBullet(_x + 28, _y + 9);
+			}
 		}
 	}
 	
-	else {
-		if (this.state === "right") {
-			this.sprite.playAnimation("forward");
-			this.state = "forward"
-		}
-	}	
-	
-	if (KEY_UP in keysDown) {
-		if (this.y > 0) {
-			this.y -= this.speed * delta;
-		}
+	//private methods
+	var getBoundingBox = function() {
+		
 	}
 	
-	if (KEY_DOWN in keysDown) {
-		if (this.y + 36 < 480) {
-			this.y += this.speed * delta;
-		}
-	}
-	
-	if (KEY_SPACE in keysDown) {
-		if (this.shotTimer <= 0) {
-			this.shotTimer = 25;
-			bulletManager.addBullet(this.x + 7, this.y + 9);
-			bulletManager.addBullet(this.x + 28, this.y + 9);
-		}
-	}
+	//Initialization
+	_sprite.addAnimation({name: "forward", speed: 0.1, frames: [1,2], loop: true});
+	_sprite.addAnimation({name: "left", speed: 0.1, frames: [4,5], loop: true});
+	_sprite.addAnimation({name: "right", speed: 0.1, frames: [7,8], loop: true});
+	_sprite.playAnimation("forward");
 }
