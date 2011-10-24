aaffe0ecb4e064cc1957bf7069dcff7b982b9b98
Collyn Philleo <jaster32@gmail.com>
2011-10-23 20:08:05 -0700

    Fixed bounding box bug if rects overlap

--- src/boundingBox.js
+++ src/boundingBox.js
@@ -1,9 +1,9 @@
 var BoundingBox = {
 	checkCollision: function(rect1, rect2) {
-		if ((rect1.x < rect2.x + rect2.width) && (rect1.x + rect1.width > rect2.x) && (rect1.y < rect2.y + rect2.height) && (rect1.y + rect1.height > rect2.y)) {
-			return true;
+		if ((rect1.x > rect2.x + rect2.width) || (rect1.x + rect1.width < rect2.x) || (rect1.y > rect2.y + rect2.height) || (rect1.y + rect1.height < rect2.y)) {
+			return false;
 		}
 		
-		return false;
+		return true;
 	}
 }
